2644f70e162bf27933e22251a0285d1749e2a323
岡田莞助 <milky4290@docomo.ne.jp>
2016-10-01 06:14:49 +0900

    プレイヤー＆カメラが少しずつ前進するスクリプトの追加

---
+++ Assets/MyFolder/script/scroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroll : MonoBehaviour {
+
+	public float scrollSpeed = 0.05f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//すこしずつ前進
+		transform.Translate (-scrollSpeed,0,0);
+	}
+}
