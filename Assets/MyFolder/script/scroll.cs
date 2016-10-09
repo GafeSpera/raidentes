6f160f6f86f4e279d4247dfe42d5e02568314dec
岡田莞助 <milky4290@docomo.ne.jp>
2016-10-09 18:02:39 +0900

    ステージが無限にスクロールする

--- Assets/MyFolder/script/scroll.cs
+++ Assets/MyFolder/script/scroll.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class scroll : MonoBehaviour {
 
+	//スクロールする速度
 	public float scrollSpeed = 0.05f;
-	// Use this for initialization
-	void Start () {
-	
-	}
+	//スクロールを繰り返す間隔
+	float size = 500f;
 
 	// Update is called once per frame
 	void Update () {
 		//すこしずつ前進
 		transform.Translate (scrollSpeed,0,0);
+		if (this.transform.position.x >= size) {
+			this.transform.Translate (-3 * size, 0, 0);
+		}
 	}
 }
