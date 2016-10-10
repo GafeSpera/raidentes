6178ca6a016ebcd6dc244ea4ee64980e1d2e9e7e
岡田莞助 <milky4290@docomo.ne.jp>
2016-10-10 10:01:41 +0900

    ステージ製作開始

--- Assets/MyFolder/script/CameraMove.cs
+++ Assets/MyFolder/script/CameraMove.cs
@@ -5,27 +5,22 @@ public class CameraMove : MonoBehaviour {
 
 	//通ってゆく地点
 	public GameObject[] pos;
-	public Vector3[] movePath;
+	private Vector3[] movePath;
 	int currentPath = 0;
 	//各地点間をいくらかけて移動すか
-	public float time = 50f;
+	public float time = 30f;
 
 	void Start () {
+		movePath = new Vector3[pos.Length];
 		for (int j = 0; j < pos.Length; j++) {
 			movePath [j] = pos [j].transform.position;
 		}
-
-		//for(int i=0; i<pos.Length; i++) {
 			iTween.MoveTo (gameObject, iTween.Hash (
 				"path", movePath,
 				"orienttopath", true,
 				"time", time,
 				"easetype", iTween.EaseType.easeInOutQuad
 			));
-			//if (pos.Length <= currentPath) {
-			//	currentPath = 0;
-			//}
-		//}
 	}
 
 	void Update () {
--- Assets/MyFolder/script/player.cs
+++ Assets/MyFolder/script/player.cs
@@ -6,12 +6,9 @@ public class player : MonoBehaviour {
 	Unit unit;
 	//移動速度
 	public float speed = 40;
-	//向きを変える速度
-	public float rotSpeed = 1.5f;
 	public int hp = 5;
 	//ダメージを受けたとき無敵時間を作る
-	int damageTime = 10;
-	bool live = true;
+	int damageTime = 60;
 	private GameObject chl;
 
 	//Startメソッドをコルーチンとして呼び出す
@@ -32,41 +29,53 @@ public class player : MonoBehaviour {
 		damageTime--;
 
 		//移動
-		float x = Input.GetAxisRaw ("Horizontal");
-		float y = Input.GetAxisRaw ("Vertical");
-		//transform.Translate (0, y * speed, x * speed);
-		//Vector3 fwd = transform.forward * x * speed;
-		//Vector3 up = transform.up * y * speed;
-		Rigidbody rb = GetComponent<Rigidbody> ();
-		rb.AddForce (transform.up * y * speed);
-		rb.AddForce (transform.forward * x * speed);
-		//rb.AddForce (fwd);
-		//rb.AddForce (up);
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		transform.Translate (0, y * speed, x * speed);
 
 	}
 
-	void OnCollisionEnter(Collision col){
+	void OnTriggerEnter(Collider col){
+		string layerName = LayerMask.LayerToName (col.gameObject.layer);
+		//衝突したものが敵以外の時は何も行わない
+		if(layerName != "Bullet(Enemy)" && layerName != "Enemy")return;
+		//衝突したものが弾だった場合、弾を削除
+		if (layerName == "Bullet(Enemy)") {
+			Destroy (col.gameObject);
+		}
 		if(damageTime <= 0){
-			string layerName = LayerMask.LayerToName (col.gameObject.layer);
-			//衝突したものが敵以外の時は何も行わない
-			if(layerName != "Bullet(Enemy)" && layerName != "Enemy")return;
-			//衝突したものが弾だった場合、弾を削除
-			if (layerName == "Bullet(Enemy)") {
-				Destroy (col.gameObject);
-			}
 			unit.Damage();
-				
 			hp --;
-			unit.Explosion ();
 			if (hp <= 0) {
+				unit.Explosion ();
 				unit.canShot = false;
 				Rigidbody rigidbody = GetComponent<Rigidbody>();
 				rigidbody.useGravity = true;
 				rigidbody.constraints = RigidbodyConstraints.None;
-				GetComponent<CapsuleCollider> ().enabled = false;
-				//Destroy (gameObject);
 			}
-			damageTime = 10;
+			damageTime = 60;
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		string layerName = LayerMask.LayerToName (col.gameObject.layer);
+		//衝突したものが敵以外の時は何も行わない
+		if(layerName != "Bullet(Enemy)" && layerName != "Enemy")return;
+		//衝突したものが弾だった場合、弾を削除
+		if (layerName == "Bullet(Enemy)") {
+			Destroy (col.gameObject);
+		}
+		if(damageTime <= 0){
+			unit.Damage();
+			hp --;
+			if (hp <= 0) {
+				unit.Explosion ();
+				unit.canShot = false;
+				Rigidbody rigidbody = GetComponent<Rigidbody>();
+				rigidbody.useGravity = true;
+				rigidbody.constraints = RigidbodyConstraints.None;
+			}
+			damageTime = 60;
 		}
 	}
 }
