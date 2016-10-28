faed00321ef2e3e4106ad81f06a5eab9032c18b8
岡田莞助 <milky4290@docomo.ne.jp>
2016-10-28 18:55:17 +0900

    no message

---
+++ Assets/MyFolder/script/AudioActive.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioActive : MonoBehaviour {
+
+	public int count = 1000;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count--;
+		if (count <= 0) {
+			audioSource.enabled = true;
+		}
+	}
+}
